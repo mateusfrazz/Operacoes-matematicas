a3f62515ac05587b42cfd8682870d438ab1433ef
mateus Frazão <>
2022-12-11 00:42:02 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (){
+
+//definindo variaveis 
+
+int a = 9, b = 7;
+
+//soma
+
+printf("\n a soma de %d e %d é = %d", a, b, a + b);
+
+//subtração
+printf("\n a subtração de %d e %d é = %d", a, b, a - b);
+
+//divisao
+
+printf("\n a divisao de %d e %d é = %d", a, b, a / b);
+
+//multiplicaçao
+
+printf("\n a multiplicacao de %d e %d é = %d", a, b, a * b);
+
+
+//resto da divisao
+
+printf("\n o resto da divisao  de %d e %d é = %d", a, b, a  % b);
+
+
+
+//valor absoluto  
+
+printf("\n o valor absoluto de -3 = %d", abs (-3));
+}
